ee74645b85636c607dd7b8b7b614b7a93c343a03
Najjashy1 <muhdabdullahi63@yahoo.com>
2023-10-19 17:20:28 -0700

    printf

---
+++ print_n.c
@@ -0,0 +1,81 @@
+#include "main.h"
+/**
+ * print_in - function that prints int
+ * @ing: an arg
+ * Return: characters number
+ */
+int print_in(va_list ing)
+{
+	int j = va_arg(ing, int), k = 1;
+	int count, ls = j % 10, integer, xp = 1;
+
+	j = j / 10;
+	count = j;
+	if (ls < 0)
+	{
+		_putchar('_');
+		 count = -count;
+		 j = -j;
+		 ls = -ls;
+		 k++;
+	}
+	if (count > 0)
+	{
+		while (count / 10 != 0)
+		{
+			xp = xp * 10;
+			count = count / 10;
+		}
+		while (xp > 0)
+		{
+			integer = count / xp;
+			_putchar(integer + '0');
+			count = count - (integer * xp);
+			xp = xp / 10;
+			k++;
+		}
+	}
+	_Putchar(ls + '0');
+	return (k);
+}
+
+#include "main.h"
+/**
+ * print_dc - function that print decimal numbers
+ * @ing: an arg
+ * Return: dec
+ */
+int print_dc(va_list ing)
+{
+	int j = va_arg(ing, int), k = 1;
+	int count, ls = j % 10, integer, xp, xp = 1;
+
+	j = j / 10;
+	count = j;
+	if (ls < 0)
+	{
+		_putchar('_');
+		count = -count;
+		j = -j;
+		ls = -ls;
+		k++;
+	}
+	if (count > 0)
+	{
+		while (count / 10 != 0)
+		{
+			xp = xp * 10;
+			count = count / 10;
+		}
+		while (xp > 0)
+		{
+			integer = count / xp;
+			_putchar(integer + '0');
+			count = count - (integer * xp);
+			xp = xp / 10;
+			k++;
+		}
+	}
+	_putchar(ls + '0');
+	return (k);
+}
